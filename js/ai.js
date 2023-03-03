f80b42d63b8def4582dfe12a0ea491bb52e3b2f0
mehedi hasan <mdmehedihasananik111@gmail.com>
2023-03-03 21:01:54 +0600

    price features information done

--- js/ai.js
+++ js/ai.js
@@ -57,6 +57,76 @@ const showAllDataCard = (cards, limitCard) => {
     spinner(false)
 }
 
+
+
+/* show modal card */
+const showModalCard = (cardId) => {
+    const URL = `https://openapi.programming-hero.com/api/ai/tool/${cardId}`
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            showModalDetails(data);
+            
+        })
+}
+
+
+/* show modal details */
+
+// const modalDetails = (detail) =>{
+//     console.log(detail.data.input_output_examples[0])
+//     const modalContainer = document.getElementById('modal-container');
+// }
+
+
+const showModalDetails = (modalDetails) => {
+    console.log(modalDetails.data.input_output_examples);
+    const modalContainer = document.getElementById('modal-container');
+    document.getElementById('modal-container').innerHTML = ''
+    modalContainer.innerHTML += `
+    <div class="card w-screen bg-red-100 shadow-xl">
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">${modalDetails.data.description}</h2>
+        <div class="flex justify-content-center gap-1 w-full">
+            <div class="bg-gray-100 rounded">
+                <p class="text-green-500">${modalDetails.data.pricing[0].price === 'No cost'  ? "Free Of cost/basic":modalDetails.data.pricing[0].price}</p>
+            </div>
+            <div class="bg-gray-100 rounded">
+                <p class="text-orange-500">${modalDetails.data.pricing[1].price === 'No cost' ? "Free Of cost/basic": modalDetails.data.pricing[1].price}</p>
+            </div>
+            <div class="bg-gray-100 rounded">
+                <p class="text-red-500">${modalDetails.data.pricing ? modalDetails.data.pricing[2].price : ""}</p>
+            </div>
+        </div>
+        <div class="flex justify-between gap-4">
+            <div>
+                <h2>Features</h2>
+                <p class="font-semibold text-gray-400">1.${modalDetails.data.features?modalDetails.data.features[1].feature_name : 'no name'} </p>
+                <p class="font-semibold text-gray-400">2.${modalDetails.data.features?modalDetails.data.features[2].feature_name : 'no name'} </p>
+                <p class="font-semibold text-gray-400">3.${modalDetails.data.features? modalDetails.data.features[3].feature_name : 'no data' } </p>
+            </div>
+            <div>
+                <h2>Integration}</h2>
+                <p class="font-semibold text-gray-400">1.${modalDetails.data.integrations[0]===undefined?'no data':modalDetails.data.integrations[0]} </p>
+                <p class="font-semibold text-gray-400">2.${modalDetails.data.integrations[1]===undefined?'no data':modalDetails.data.integrations[1]} </p>
+                <p class="font-semibold text-gray-400">3.${modalDetails.data.integrations[2]===undefined?'no data':modalDetails.data.integrations[2]} </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card w-screen bg-base-100 shadow-xl">
+    <div class="px-10 pt-10">
+    <figure><img src="${modalDetails.data.image_link[0]}" alt="Shoes" /></figure>
+    </div>
+    <div class="card-body items-center text-center">
+        <h4 class="text-2xl font-semibold">${modalDetails.data.input_output_examples[0].input}</h4>
+        <p>${modalDetails.data.input_output_examples[0].output}</p>
+    </div>
+</div>
+    `
+}
+
+
 /* spinner function */
 const spinner = (isLoading) => {
     const toggleSpinner = document.getElementById('spinner');
@@ -66,68 +136,6 @@ const spinner = (isLoading) => {
         toggleSpinner.classList.add('d-none')
     }
 }
-
-/* show modal card */
-const showModalCard = (cardId) => {
-    const URL = `https://openapi.programming-hero.com/api/ai/tool/${cardId}`
-    fetch(URL)
-        .then(res => res.json())
-        .then(data => {
-            modalDetails(data.data);
-        })
-}
-
-
-/* show modal details */
-const modalDetails = (modalDetails) => {
-    console.log(modalDetails);
-    const modalContainer = document.getElementById('modal-container');
-    document.getElementById('modal-container').innerHTML = ''
-    modalContainer.innerHTML += `
-    <div class="card w-screen bg-red-100 shadow-xl">
-    <div class="card-body items-center text-center">
-        <h2 class="card-title">${modalDetails.description}</h2>
-        <div class="flex gap-2 font-semibold">
-            <div class="bg-gray-100 p-1 rounded">
-                <p class="text-green-500">$10/ month basic</p>
-            </div>
-            <div class="bg-gray-100 p-1 rounded">
-                <p class="text-orange-500">$50/ month pro</p>
-            </div>
-            <div class="bg-gray-100 p-1 rounded">
-                <p class="text-red-500">Contact us EnterPrise</p>
-            </div>
-        </div>
-        <div class="flex justify-between gap-4">
-            <div>
-                <h2>Features</h2>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.features[1].feature_name}</p>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.features[2].feature_name}</p>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.features[3].feature_name}</p>
-            </div>
-            <div>
-                <h2>Integration}</h2>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.integrations ? modalDetails.integrations[0] : 'No Data Found'}</p>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.integrations ? modalDetails.integrations[1] : 'No Data Found'}</p>
-                <p class="font-semibold text-gray-400">1. ${modalDetails.integrations ? modalDetails.integrations[2] : 'No Data Found'}</p>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card w-screen bg-base-100 shadow-xl">
-    <div class="px-10 pt-10">
-    <figure><img src="${modalDetails.image_link[0]}" alt="Shoes" /></figure>
-    </div>
-    <div class="card-body items-center text-center">
-        <h4 class="text-2xl font-semibold">${modalDetails.input_output_examples === true ? modalDetails.input_output_examples[0].input : "Can you give any example?"}</h4>
-        <p>${modalDetails.input_output_examples ? modalDetails.input_output_examples[0].output : "Not! Not Yet! Take a break!!!"}</p>
-    </div>
-</div>
-    `
-}
-
-
-
 
 document.getElementById('see-more-btn').addEventListener('click', function () {
     allFetchData()
